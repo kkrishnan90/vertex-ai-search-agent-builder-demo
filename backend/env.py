a6bad0e1556a93e67b76899fc01b0905349ce1dd
Krishnan Kumar <kkrish@google.com>
2024-06-20 18:31:37 +0000

    env:load

--- backend/env.py
+++ backend/env.py
@@ -11,6 +11,20 @@ BUCKET_NAME = os.getenv("BUCKET_NAME")
 DATASTORE_ID = os.getenv("DATASTORE_ID")
 AGENT_APPLICATION_ID = os.getenv("AGENT_APPLICATION_ID")
 
+
+# Check if environment variables are empty and load from Cloud Run if necessary
+if not PROJECT_ID:
+    PROJECT_ID = os.getenv("PROJECT_ID", None)
+if not LOCATION:
+    LOCATION = os.getenv("LOCATION", None)
+if not BUCKET_NAME:
+    BUCKET_NAME = os.getenv("BUCKET_NAME", None)
+if not DATASTORE_ID:
+    DATASTORE_ID = os.getenv("DATASTORE_ID", None)
+if not AGENT_APPLICATION_ID:
+    AGENT_APPLICATION_ID = os.getenv("AGENT_APPLICATION_ID", None)
+
+
 @log_data
 def show_all_env():
     """
